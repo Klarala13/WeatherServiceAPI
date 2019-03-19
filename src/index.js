100b7fcf219db2620ae5abde0860d04c68ba3ff4
Klara <sanchez.clara13@gmail.com>
2019-03-19 10:37:19 +0100

    left only backend

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import weatherClient from 'lib/WeatherClient';
+import WeatherApi from "lib/WeatherApi";
 require("dotenv").config();
 
-const weather = new WeatherAPI(process.env.API_KEY);
+const weather = new WeatherApi(process.env.API_KEY);
 const city = process.argv[2].trim();
 const country = process.argv[3].trim();
 const zip = process.argv[2].trim();
-const zipRegex = /\d{4,6}/g;
 
 if (!city) {
   console.log("Not a valid City");
@@ -22,6 +21,5 @@ const currentWeather = async (zip, country) => {
   const nowWeather = await weather.getData(zip, country);
   console.log("nowWeather:", nowWeather);
 }
-currentWeather (zip, country);
 
-document.querySelector("#main").textContent =  `The weather in ${this.city} currently is ${currentWeather()}`
+currentWeather (zip, country);
